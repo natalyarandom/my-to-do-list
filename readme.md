78fbca85a480ced9cc633c20443c109f91bc9333
natalyarandom <llevella89@gmail.com>
2022-03-26 18:05:00 +0300

    change the name

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-# to do list
+# my-to-do-list
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+< !DOCTYPE html >
+    <html>
+        <head>
+
+            <h1>To do list</h1>
+
+        </head>
+
+        <body>
+
+
+
+
+
+
+
+
+
+        </body>
+
+
+
+
+
+
+
+    </html>
